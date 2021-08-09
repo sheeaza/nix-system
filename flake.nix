5b2a34e0ee1649da14254a606f770688471cc536
sheeaza <winsdenny@outlokk.com>
2021-08-10 00:14:05 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.05;
+    nixpkgs-unstable.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+    nvim.url = github:sheeaza/nix-nvim;
+  };
+
+  outputs = { self, ... }@inputs: {
+    nixosConfigurations.max = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [ 
+        ./configuration.nix
+      ];
+    };
+  };
+}
